9d4c69bca577f1c456cbf4de617c50937c88030c
josex2r <jose.represa@beeva.com>
2017-11-02 20:27:40 +0100

    testing cron response

--- skills/channel_join.js
+++ skills/channel_join.js
@@ -1,13 +1,22 @@
 var debug = require('debug')('botkit:channel_join');
+const schedule = require('node-schedule');
+const { promisify } = require('util');
+
+async function dailyResume(controller, bot, message) {
+  const text = 'Hola k ase, aquí hay que mostrar el número de issues abiertas';
+
+  bot.reply(message, text);
+}
 
 module.exports = function(controller) {
+  controller.on('bot_channel_join', async function(bot, message) {
+    const dailyReportSchedule = schedule.scheduleJob('*/10 * * * * *', dailyResume.bind(this, controller));
 
-    controller.on('bot_channel_join', function(bot, message) {
-
-        controller.studio.run(bot, 'channel_join', message.user, message.channel).catch(function(err) {
-            debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
-        });
-
-    });
-
-}
+    // Notify studio
+    try {
+      await controller.studio.run(bot, 'channel_join', message.user, message.channel);
+    } catch (err) {
+      debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
+    }
+  });
+};
