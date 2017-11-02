0c52d75dd9d88ba3b92d62c97c08e51ef44ed689
josex2r <jose.represa@beeva.com>
2017-11-02 21:05:02 +0100

    store channels

--- skills/channel_join.js
+++ skills/channel_join.js
@@ -1,8 +1,7 @@
-var debug = require('debug')('botkit:channel_join');
+const debug = require('debug')('botkit:channel_join');
 const schedule = require('node-schedule');
-const { promisify } = require('util');
 
-async function dailyResume(controller, bot, message) {
+function dailyResume(controller, bot, message) {
   const text = 'Hola k ase, aquí hay que mostrar el número de issues abiertas';
 
   bot.reply(message, text);
@@ -10,14 +9,39 @@ async function dailyResume(controller, bot, message) {
 }
 
 module.exports = function(controller) {
-  controller.on('bot_channel_join', async function(bot, message) {
-    const dailyReportSchedule = schedule.scheduleJob('*/10 * * * * *', dailyResume.bind(this, controller, bot, message));
+  controller.on('bot_channel_join', function(bot, message) {
 
     // Notify studio
-    try {
-      await controller.studio.run(bot, 'channel_join', message.user, message.channel);
-    } catch (err) {
+    controller.studio.run(bot, 'channel_join', message.user, message.channel).catch((err) => {
       debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
-    }
+    });
+
+    controller.storage.channel.get('schedule', (err, list) => {
+      if (!list || !list.length) {
+        list = [];
+      }
+
+      if (list.contains(message.channel)) {
+        return;
+      }
+
+      list.push(message.channel);
+
+      controller.storage.channel.save(list, (err2/* , saved */) => {
+        if (err2) {
+          bot.reply(message, 'Some error has ocurred, the channel will not receive any report :(');
+          debug(`I experienced an error adding your task: ${err}`);
+        } else {
+          bot.reply(message, 'Channel is ready to receive reports');
+          bot.api.reactions.add({
+            name: 'thumbsup',
+            channel: message.channel,
+            timestamp: message.ts
+          });
+        }
+      });
+    });
+
+    // schedule.scheduleJob('*/10 * * * * *', dailyResume.bind(this, controller, bot, message));
   });
 };
