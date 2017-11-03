4b22fbc8be0d77bb69a29e86b702315a3d8afa2c
josex2r <jose.represa@beeva.com>
2017-11-03 09:19:33 +0100

    connect to RTM

--- skills/channel_join.js
+++ skills/channel_join.js
@@ -1,41 +1,49 @@
 const debug = require('debug')('botkit:channel_join');
 const schedule = require('node-schedule');
 
-function sendDailyResume(bot, channelId) {
+function sendDailyResume(controller, id, token) {
   const text = 'Hola k ase, aquí hay que mostrar el número de issues abiertas';
-  
-  console.log({text, channelId})
-  bot.say({
-    text,
-    channel: channelId
+
+  controller.spawn({ token }).startRTM((err, bot) => {
+    if (err) {
+      return;
+    }
+
+    console.log({ text, id })
+    bot.say({
+      text,
+      channel: id
+    });
   });
 }
 
 module.exports = function(controller) {
-  controller.middleware.receive.use(function(bot, message, next) {
-
-    // do something...
-    console.log('RCVD:', message, bot);
-    next();
-
-  });
 
   // Wake up timers
-  controller.on('create_bot', (bot/* , config */) => {
-    controller.storage.channels.all((err, channels) => {
-      channels = err ? [] : channels.filter((channel) => channel.notifications);
-      channels.forEach((channel) => {
-        bot.startRTM((err2) => {
-          if (err2) {
-            debug('Error creating cron');
-          }
+  // controller.on('create_bot', (bot/* , config */) => {
+    controller.storage.teams.all((err, teams) => {
+      if (err || !teams) {
+        return;
+      }
 
-          sendDailyResume(bot, channel.id);
-        });
+      const subscriptions = teams.reduce((acc, team) => {
+        if (team.subscriptions) {
+          team.subscriptions = team.subscriptions.map((id) => ({
+            id,
+            token: team.bot.token
+          }));
+
+          return acc.concat(...team.subscriptions);
+        }
+
+        return acc;
+      }, []);
+
+      subscriptions.forEach(({ id, token }) => {
+        sendDailyResume(controller, id, token);
       });
     });
-    
-  });
+  // });
 
   controller.on('bot_channel_join', function(bot, message) {
     // Notify studio
@@ -43,20 +51,20 @@ module.exports = function(controller) {
       debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
     });
 
-    controller.storage.channels.get(message.channel, (err, channel) => {
-      if (!channel) {
-        channel = {};
-        channel.id = message.channel;
+    controller.storage.teams.get(message.team, (err, team) => {
+      if (!team) {
+        return;
       }
 
-      channel.notifications = true;
+      team.subscriptions = team.subscriptions || [];
+      team.subscriptions.push(message.team);
 
-      controller.storage.channels.save(channel, (err2/* , saved */) => {
+      controller.storage.channels.save(team, (err2/* , saved */) => {
         if (err2) {
           bot.reply(message, 'Some error has ocurred, the channels will not receive any report :(');
           debug(`I experienced an error adding your task: ${err}`);
         } else {
-          bot.reply(message, 'Channel is ready to receive reports');
+          bot.reply(message, 'Channel is ready to receive reports :)');
           bot.api.reactions.add({
             name: 'thumbsup',
             channel: message.channel,
