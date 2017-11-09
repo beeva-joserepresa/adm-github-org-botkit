7b5d84cbb00f8405d73b6bf1ea809af4d685bd9a
josex2r <jose.represa@beeva.com>
2017-11-09 21:57:48 +0100

    fix help

--- skills/intent_dispatcher.js
+++ skills/intent_dispatcher.js
@@ -13,12 +13,24 @@ module.exports = function(controller) {
       return true;
     }
 
+    console.log(message.topIntent)
+
     switch (message.topIntent.intent) {
     case intents.listIssues:
       listIssues(controller, bot, message);
       break;
     case intents.menu:
+    case intents.hello:
       handleMenu(controller, bot, message);
+      break;
+    case intents.help:
+      const cmd = message.text.replace(/help\s?/, '');
+
+      if (cmd) {
+        controller.studio.run(bot, `help_${cmd}`, message.user, message.channel);
+      } else {
+        controller.studio.run(bot, 'help', message.user, message.channel);
+      }
       break;
     default:
       bot.reply(message, 'Sorry, I don\'t know what it means :(');
